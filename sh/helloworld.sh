591a425e7dcf7c738a0a8d4de524a8a94c082e37
Gioxx <giovanni@solone.it>
2021-01-06 12:57:19 +0100

    IMACGLN

--- sh/helloworld.sh
+++ sh/helloworld.sh
@@ -3,10 +3,10 @@ mese=$(date +%m)
 anno=$(date +%Y)
 ora=$(date +%H)
 minuto=$(date +%M)
-update=$anno$mese$giorno$ora$minuto
-lastmodified=$giorno"-"$mese"-"$anno
-echo "{GHA_update}={$update}" >> $GITHUB_ENV
-echo "{GHA_lastmodified}={$lastmodified}" >> $GITHUB_ENV
+#update=$anno$mese$giorno$ora$minuto
+#lastmodified=$giorno"-"$mese"-"$anno
+echo "update=$anno$mese$giorno$ora$minuto" >> $GITHUB_ENV
+echo "lastmodified=$giorno"-"$mese"-"$anno" >> $GITHUB_ENV
 
 #cd contrib
 #for s in xfiles_*; do (head -n 1 "${s}" && tail -n +2 "${s}" | sort) > sorted_${s}; done
