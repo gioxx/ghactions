d3a5c0a29d745999a637db8996c327630bdbc658
Gioxx <giovanni@solone.it>
2021-01-06 14:31:20 +0100

    Update helloworld.sh

--- sh/helloworld.sh
+++ sh/helloworld.sh
@@ -5,9 +5,4 @@ ora=$(date +%H)
 minuto=$(date +%M)
 echo "update=$anno$mese$giorno$ora$minuto" >> $GITHUB_ENV
 echo "lastmodified=$giorno"-"$mese"-"$anno" >> $GITHUB_ENV
-
-#cd contrib
-#for s in xfiles_*; do (head -n 1 "${s}" && tail -n +2 "${s}" | sort) > sorted_${s}; done
-#	for f in sorted_*; do (cat "${f}") >> experimental.txt; done
-
 exit
